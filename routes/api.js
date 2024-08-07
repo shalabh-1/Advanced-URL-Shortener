ab3f00041346953c067aba587e41ae47662dbce8
shalabh-1 <lemonboy242@gmail.com>
2024-08-07 11:01:25 +0530

    Tested OK

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,7 @@ const { generateNewShortUrl, redirectToOriginalUrl, handleGetAnalytics } = requi
 const radiscache=require("../config/radiscacheconfig.js")
 // Define routes
 router.post("/shortid", radiscache.route(),generateNewShortUrl);
-router.get('/redirect/:shortId', radiscache.route(), (req, res, next) => {
-    console.log('Redirecting to original URL');
-    redirectToOriginalUrl(req, res, next);
-});
+router.get('/redirect/:shortId', radiscache.route(),redirectToOriginalUrl);
 router.get("/analytics/:shortId", radiscache.route(),handleGetAnalytics);
 
 module.exports = router;
